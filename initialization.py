ad25d5ef1ded4563e534fe3e86e50bf545032399
Bluewhale97 <71245576+Bluewhale97@users.noreply.github.com>
2021-09-08 11:46:52 -0400

    Update initialization.py

--- initialization.py
+++ initialization.py
@@ -96,10 +96,8 @@ def initialize_parameters_zeros(layers_dims):
     L = len(layers_dims)  # number of layers in the network
 
     for l in range(1, L):
-        ### START CODE HERE ### (≈ 2 lines of code)
         parameters['W' + str(l)] = np.zeros((layers_dims[l], layers_dims[l - 1]))
         parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
-        ### END CODE HERE ###
     return parameters
 
 
@@ -121,10 +119,9 @@ def initialize_parameters_random(layers_dims):
     L = len(layers_dims)  # integer representing the number of layers
 
     for l in range(1, L):
-        ### START CODE HERE ### (≈ 2 lines of code)
+
         parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * 10
         parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
-        ### END CODE HERE ###
 
     return parameters
 
@@ -147,10 +144,8 @@ def initialize_parameters_he(layers_dims):
     L = len(layers_dims) - 1  # integer representing the number of layers
 
     for l in range(1, L + 1):
-        ### START CODE HERE ### (≈ 2 lines of code)
         parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * np.sqrt(2./layers_dims[l - 1])
         parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
-        ### END CODE HERE ###
 
     return parameters
 
